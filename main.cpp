f27876f117cd127223cb898dc096b2cdb64f14bf
Olha <olhalashchukevych@gmail.com>
2024-03-15 01:34:07 -0500

    Add main.cpp file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "Select a menu option: " << std::endl;
+    std::cout << "   1. Display general grocery stores information" << std::endl;
+    std::cout << "   2. Display neighborhood charts" << std::endl;
+    std::cout << "   3. Find closest stores" << std::endl;
+    std::cout << "   4. Search for store by name" << std::endl;
+    std::cout << "   5. Exit" << std::endl;
+    std::cout << "Your choice: ";
+
+    return 0;
+}
