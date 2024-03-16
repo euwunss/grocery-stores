fbe057786201e4556c3cf37a5a8dd81031387232
Olha <olhalashchukevych@gmail.com>
2024-03-16 15:23:44 -0500

    Add prompt for user's choice

--- main.cpp
+++ main.cpp
@@ -1,14 +1,46 @@
 #include <iostream>
+#include <string>
+
+// Function prototypes
+int getUserChoice();
 
 int main() {
+    int userChoice = getUserChoice(); 
 
-    std::cout << "Select a menu option: " << std::endl;
-    std::cout << "   1. Display general grocery stores information" << std::endl;
-    std::cout << "   2. Display neighborhood charts" << std::endl;
-    std::cout << "   3. Find closest stores" << std::endl;
-    std::cout << "   4. Search for store by name" << std::endl;
-    std::cout << "   5. Exit" << std::endl;
-    std::cout << "Your choice: ";
+    while (userChoice != 5) {
+        switch(userChoice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+
+        userChoice = getUserChoice();
+    }
 
     return 0;
+}
+
+int getUserChoice() {
+    int choice;
+
+    do {
+        std::cout << "Select a menu option: " << std::endl;
+        std::cout << "   1. Display general grocery stores information" << std::endl;
+        std::cout << "   2. Display neighborhood charts" << std::endl;
+        std::cout << "   3. Find closest stores" << std::endl;
+        std::cout << "   4. Search for store by name" << std::endl;
+        std::cout << "   5. Exit" << std::endl;
+        std::cout << "Your choice: ";
+
+        std::cin >> choice;
+    } while (choice < 1 || choice > 5);
+
+    return choice;
 }
