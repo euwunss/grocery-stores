657a9647df8cde1490d64f93e36114a79b007ceb
Olha <olhalashchukevych@gmail.com>
2024-03-17 02:34:56 -0500

    Add reading grocery store data from file
    - Implemented `readFile` function to read data about grocery stores from a user-specified file
    - Reads each line of the file, parsing the data to create Store objects
    - Populates stores vector with the created Store objects
    - Added error handling to handle cases where the file cannot be opened or data cannot be read

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include "store.h"
 
 // Function prototypes
 int getUserChoice();
+std::string getUserDatafile();
+void readFile(std::string fileName, std::vector<Store>& stores);
 
 int main() {
+    std::vector<Store> stores;
+    
+    std::string fileName = getUserDatafile();
+    readFile(fileName, stores);
     int userChoice = getUserChoice(); 
 
     while (userChoice != 5) {
@@ -25,6 +36,56 @@ int main() {
     }
 
     return 0;
+}
+
+std::string getUserDatafile() {
+    std::string fileName;
+
+    std::cout << "Enter filename: ";
+    std::cin >> fileName;
+
+    return fileName;
+}
+
+void readFile(std::string fileName, std::vector<Store>& stores) {
+    std::string input, line;
+    std::string name, size, address, neighbourhood;
+    int sqftSize;
+    double latitude, longtitude;
+
+    std::ifstream fileIn;
+    fileIn.open("datafiles/" + fileName);
+    if (!fileIn.is_open()) {
+        std::cout << "Error opening this file." << std::endl;
+        exit(-1);
+    }
+
+    while (getline(fileIn, line)) {
+        std::vector<std::string> data;
+        std::istringstream lineIn(line);
+
+        while (getline(lineIn, input, ',')) {
+            data.push_back(input);
+        }
+
+        if (data.size() == 7) {
+            name = data[0];
+            sqftSize = stoi(data[1]);
+            size = data[2];
+            address = data[3];
+            neighbourhood = data[4];
+            latitude = stod(data[5]);
+            longtitude = stod(data[6]);
+
+            Store groceryStore(name, sqftSize, size, address, neighbourhood, latitude, longtitude);
+            stores.push_back(groceryStore);
+        }
+        else {
+            std::cout << "Error: Information missing." << std::endl;
+            return;
+        }
+    }
+    fileIn.close();
 }
 
 int getUserChoice() {
